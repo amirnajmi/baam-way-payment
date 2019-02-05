a4e71e9679a93f3e2f24042ded9bfc18141f3d27
shgholami <shahrbanoo.gholami@gmail.com>
2019-02-05 15:15:47 +0330

    FinancialAmountsRepository and Service added

---
+++ src/main/java/ir/co/sadad/eb/repository/api/FinancialAmountsRepository.java
@@ -0,0 +1,8 @@
+package ir.co.sadad.eb.repository.api;
+
+import ir.co.sadad.eb.domain.FinancialAmounts;
+import org.apache.deltaspike.data.api.Repository;
+
+@Repository
+public interface FinancialAmountsRepository extends IRepository<FinancialAmounts,Long>{
+}
---
+++ src/main/java/ir/co/sadad/eb/service/api/IFinancialAmountsService.java
@@ -0,0 +1,6 @@
+package ir.co.sadad.eb.service.api;
+
+import ir.co.sadad.eb.domain.FinancialAmounts;
+
+public interface IFinancialAmountsService extends IService<FinancialAmounts, Long> {
+}
